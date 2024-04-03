ae726401990b6d2b9b964b2aa25648b2621f79bd
GabrielSampaioC <gabrielsampaio207@gmail.com>
2024-04-03 19:12:03 -0300

    fix: ajustando o menu versão mobile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
+  }
 })
+
